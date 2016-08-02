36f108d3b4041aba018797aa07dda039547ece9b
jakemat29 <jakemat29@gmail.com>
2016-08-03 01:10:55 +0530

    Revert "this is my first commit in this repo... studying python"
    This reverts commit 6f31282fcffde6299309c082713ce4ee98e5b50a.

--- ex1.py
+++
@@ -1,9 +0,0 @@
-print "helloworld!"
-print "hello again"
-print "yo yo";
-print "this is fun."
-print 'yay ! printing'
-print "I'd much rather you 'not'."
-#print 'I "said" do not touch this'       # used for comments
-print "hello # there"
-
