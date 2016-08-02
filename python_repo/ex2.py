dc462f9099e509ef4333dca04dca382767cd93da
jakemat29 <jakemat29@gmail.com>
2016-08-03 01:22:33 +0530

    this is my first commit in this repo... studying python

---
+++ python_repo/ex2.py
@@ -0,0 +1,11 @@
+print "i'll count the  chickens"
+print "chickens=", 25 + 30 / 6   #30/6=5->25+5=30
+print "roosters=", 100 - 25 * 3 % 4    #25*3=75 -> 75%4=3 -> 100-3=97
+print "is it true 3+5>7-2", 3 + 5 > 7 - 2  #   <, >, <=, >= returns boolean values
+print "PEDMAS applies here", 3 * (82%7) / 2    # PEDMAS applies here  *P= Parenthesis *E= Exponents///here the execution is like this 
+																	# 82%7= 5 -> 3*5=15 -> 15/2=7
+print "PEDMAS applies here with float valuess", 3.0 * (82%7) / 2    # PEDMAS applies here ///here the execution is like this 
+																	# 82%7= 5 -> 3*5=15 -> 15/2=7.5
+print 5 - 7
+print 5-2 >= 5
+print  3.5 * 3
---
+++ python_repo/ex3.py
@@ -0,0 +1,11 @@
+cars = 100
+drivers = 30
+car_capacity = 4
+no_of_cars_driven = drivers
+print " No of cars driven = ", no_of_cars_driven
+no_of_cars_not_driven = cars - drivers
+print " No of cars NOT driven = ", no_of_cars_not_driven
+passengers = 90
+passengers_per_car = passengers / no_of_cars_driven
+print "passengers per car= ", passengers_per_car
+print "hey %s there." % "you"       # just see the results %s replaced by 'you'
